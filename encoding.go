3c05318f3be13665948292be54f9aeae1eff5d62
six <scirocco6@gmail.com>
2015-07-07 15:47:32 -0700

    formatting

--- encoding.go
+++ encoding.go
@@ -1,5 +1,6 @@
 package icb
 
+// CreatePacket is the primary way to make icb packets for transmission
 func CreatePacket(kind string, parameters ...string) Packet {
 	var packet Packet
 
@@ -8,42 +9,34 @@ func CreatePacket(kind string, parameters ...string) Packet {
 		{
 			packet.Init([]byte{'a'}, parameters)
 		}
-
 	case "beep":
 		{
 			packet.Init([]byte{'h'}, []string{"beep", parameters[0]})
 		}
-
 	case "public":
 		{
 			packet.Init([]byte{'b'}, parameters)
 		}
-
 	case "private":
 		{
 			packet.Init([]byte{'h'}, []string{"m", parameters[0] + " " + parameters[1]})
 		}
-
 	case "join":
 		{
 			packet.Init([]byte{'h'}, []string{"g", parameters[0]})
 		}
-
 	case "global_who":
 		{
 			packet.Init([]byte{'h'}, []string{"w\001"})
 		}
-
 	case "local_who":
 		{
 			packet.Init([]byte{'h'}, append([]string{"w"}, parameters...))
 		}
-
 	case "nop":
 		{
 			packet.Init([]byte{'n'}, parameters)
 		}
 	}
-
 	return packet
 }
